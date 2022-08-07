7c7df6f3cb73f30e5c999f243d664753d62148a1
Josias Hod <55164489+josiashod@users.noreply.github.com>
2022-08-07 18:49:08 +0100

    Move to classes attributes

--- models/place.py
+++ models/place.py
@@ -21,6 +21,17 @@ class Place(base_model.BaseModel):
         amenity_ids (list): lists of amenity of the place.
     """
 
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
     def __init__(self, *args, **kwargs):
         """Initialize the class.
 
@@ -28,17 +39,6 @@ class Place(base_model.BaseModel):
             args (list): not used.
             kwargs (dict): used to assign attribute to the object
         """
-
-        self.city_id = ""
-        self.user_id = ""
-        self.name = ""
-        self.description = ""
-        self.number_rooms = 0
-        self.max_guest = 0
-        self.price_by_night = 0
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.amenity_ids = []
 
         if kwargs is not None and len(kwargs.keys()) > 0:
             super().__init__(**kwargs)
